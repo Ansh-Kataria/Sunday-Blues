a75cca327b21eef4227361eba1788700ef4f9c86
Ansh Kataria <anshkataria28@gmail.com>
2023-01-23 01:05:48 +0530

    header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import "./Header.css"
-import {logo} from "../../assets"
+import React from "react";
+import "./Header.css";
+import { logo } from "../../assets";
 
 const Header = () => {
   return (
-    <div className='header'>
-        <div className="header-left">
-            <div className="header-left-text">
-                <span>SUNDAY BLUES</span>
-            </div>
-            <img src={logo} alt="logo" />
+    <div className="header">
+      <div className="header-left">
+        <div className="header-left-text">
+          <span>SUNDAY BLUES</span>
         </div>
-        <div className="header-right">
-            <p>DISCO CLUB</p>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="header-right">
+        <div className="header-right-text">
+          <p>DISCO CLUB</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
