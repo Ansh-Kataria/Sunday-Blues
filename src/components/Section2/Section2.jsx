9ce5e61d1c3b62b24580238729aa285489a48d2a
Ansh Kataria <anshkataria28@gmail.com>
2023-01-25 23:43:49 +0530

    Section2 updated

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { section2 } from "../../assets";
-import "./Section2.css"
+import "./Section2.css";
 
 const Section2 = () => {
   return (
@@ -14,9 +14,11 @@ const Section2 = () => {
         </div>
         <div className="section2-right-text">
           <p>
-            O serviço de assinatura mensal do clube oferece aos amantes da
-            música a oportunidade de receber discos de vinil exclusivos e
-            limitados diretamente em suas casas todos os meses.
+            Os álbuns são de artistas consagrados e também de novos talentos,
+            garantindo uma experiência musical eclética e sempre emocionante.
+            Além disso, os assinantes têm acesso a conteúdo exclusivo, como
+            entrevistas com os artistas e sessões de perguntas e respostas com a
+            equipe de curadores.
           </p>
         </div>
       </div>
